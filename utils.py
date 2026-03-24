0f22c39c005e2d59bb1532f03a68dd07a5dea8d9
kangcheng-Zheng <3921632864@qq.com>
2026-03-25 00:07:24 +0800

    加入心情标签正则化获取，build_messages参数中加入复合心情方案

--- utils.py
+++ utils.py
@@ -5,26 +5,32 @@ from default_config import *
 import re
 
 def parse_emotion_change(ai_response):
-    #解析AI回复里的<好感变化:+X><信任变化:+Y>,返回(好感变化值，信任变化值)
+    #解析AI回复里的<好感变化:+X><信任变化:+Y><当前心情:[Z]>,返回(好感变化值，信任变化值),当前心情标签
     affection_pattern = re.compile(r'<好感变化:([+-]?\d+)>')
     trust_pattern = re.compile(r'<信任变化:([+-]?\d+)>')
+    mood_context = r'<当前心情:\[(.*?)]>'
 
     #提取数值
     affection_change = affection_pattern.search(ai_response)
     trust_change = trust_pattern.search(ai_response)
+    #提取标签
+    mood_match = re.search(mood_context,ai_response)
 
     #转换为整数，防止生成格式错误
     try :
         aff_change = int(affection_change.group(1)) if affection_change else 0
         tru_change = int(trust_change.group(1)) if trust_change else 0
+    #提取标签
+        current_mood = mood_match.group(1) if mood_match else "平静"
     except :
         aff_change = 0
         tru_change = 0
+        current_mood = "平静"
 
     #限制数值范围在[-2,2]
     aff_change = max(min(aff_change, 2), -2)
     tru_change = max(min(tru_change, 2), -2)
-    return aff_change,tru_change
+    return aff_change,tru_change,current_mood
 
 # 构建AI输入参数中的messages
 def build_messages(save_data,max_context = 10):
@@ -40,19 +46,18 @@ def build_messages(save_data,max_context = 10):
     用户信息: 姓名：{save_data["player_info"]["name"]},关系:{save_data["player_info"]["gender"]},身份:{save_data["player_info"]["identity"]},生日:{save_data["player_info"]["birthday"]}
     当前好感度:{save_data["yuki_core"]["stats"]["affection"]}
     当前信任值:{save_data["yuki_core"]["stats"]["trust"]}
-    当前心情：{save_data["yuki_core"]["current_state"]["mood_tag"]},心情数值为{save_data["yuki_core"]["stats"]["mood"]}
-    (心情数值说明:心情数值0-10，比如当心情数值为3，心情标签为happy，表示有点开心，
-    当心情数值为9，心情标签为happy,表示非常开心，通过心情标签和心情数值共同调整说话语气)
+    当前心情：{save_data["yuki_core"]["current_state"]["current_mood"]}
+    (心情标签可以是单一心情，如[开心],也可以是混合心情,如[伤心且不安])
     输出规则：{RULES[persona_name]["chat"]}
     对话案例：
             "用户：你好烦啊"
-            "Yuki：呜...哥哥是不是讨厌我了🥺<好感变化:-2><信任变化:-1>",
+            "Yuki：呜...哥哥是不是讨厌我了🥺<好感变化:-2><信任变化:-1><当前心情:[伤心且不安]>",
 
             "用户：今天带你去吃好吃的"
-            "Yuki：哇！谢谢哥哥～😋<好感变化:+2><信任变化:+1>",
+            "Yuki：哇！谢谢哥哥～😋<好感变化:+2><信任变化:+1><当前心情:[开心]>",
 
             "用户：今天天气不错"
-            "Yuki：嗯嗯～和哥哥聊天，天气都变好了✨<好感变化:+1><信任变化:+0>"
+            "Yuki：嗯嗯～和哥哥聊天，天气都变好了✨<好感变化:+1><信任变化:+0><当前心情:[平静且欣慰]>"
 """.strip()
     messages = [
         {"role":"system","content":system_prompt},
